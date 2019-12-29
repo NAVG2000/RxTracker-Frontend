bee64894b24146290220552a820f8360c32b7980
NAVG2000 <navg2000@google.com>
2019-12-29 16:39:14 -0500

    Change index.jsx to main.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.jsx',
+    entry: './src/main.jsx',
     output: {
-        filename: 'index-[contentHash].js',
+        filename: 'main-[contentHash].js',
         path: path.join(__dirname, '/dist')
     },
     module: {
