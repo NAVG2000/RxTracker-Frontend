aac3ee7ddc39703c1a8e96d5f3744e001bd127ca
NAVG2000 <navg2000@gmail.com>
2020-07-18 20:38:20 -0400

    test7

--- src/header.tsx
+++ src/header.tsx
@@ -13,7 +13,7 @@ class Header extends React.Component<HearderProps, HeaderState>{
 
     render() {
         return <div id='headerContainer'>
-            Rx Tracker Test6
+            Rx Tracker Test7
         </div>
     }
 }
